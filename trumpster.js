871b7acb97ef8a8f9db4d75477a85b8da74e4fbc
mralexlau <laux0083@gmail.com>
2015-02-27 07:51:12 -0600

    Update card styles

--- trumpster.js
+++ trumpster.js
@@ -15,23 +15,61 @@ if (Meteor.isClient) {
       if (player2.get() !== null) {
         return player2.get().hand;
       }
-    }
+    },
   });
 
   Template.currentPlayer.events({
     'click .players-cards.card': function (event) {
       var $card = $($(event.target).parents('.card')),
           $opponentsCard,
-          winningCard;
+          winningCard,
+          player1Hand,
+          player2Hand,
+          cardFinder,
+          numberOfCards;
 
       if ($card.length > 0) {
+        numberOfCards = $('.players-cards.card').length;
         $opponentsCard = $('.opponent .card').random();
 
-        Trumpster.Animations.opponentsCardToMiddle($opponentsCard);
-        Trumpster.Animations.playersCardToMiddle($card);
+    $('.current-player .card').each(function(index) {
+      $(this).data('position', index);
+    });
+    $('.opponent .card').each(function(index) {
+      $(this).data('position', index);
+    });
+
+        Trumpster.Animations.opponentsCardToMiddle($opponentsCard, numberOfCards);
+        Trumpster.Animations.playersCardToMiddle($card, numberOfCards);
 
         winningCard = Trumpster.RuleMaster.winningCard($card, $opponentsCard);
         Trumpster.Animations.showAsWinner(winningCard);
+
+
+        setTimeout(function () {
+          player1Hand = player1.get().hand;
+          player2Hand = player2.get().hand;
+
+          player1CardFinder = function (card) {
+            return card.suit == $card.data('suit') && card.value == $card.data('rank');
+          };
+
+          opponentCardFinder = function (card) {
+            return card.suit == $opponentsCard.data('suit') && card.value == $opponentsCard.data('rank');
+          };
+
+          //index1 = player1Hand.indexOf(_.find(player1Hand, player1CardFinder));
+          //if (index1 >= 0) {
+            //player1Hand.splice(index1, 1);
+            //player1.set({ hand: player1Hand });
+          //}
+
+          //index2 = player2Hand.indexOf(_.find(player2Hand, opponentCardFinder));
+          //if (index2 >= 0) {
+            //player2Hand.splice(index2, 1);
+            //player2.set({ hand: player2Hand });
+          //}
+        }, 2000);
       }
     }
   });
